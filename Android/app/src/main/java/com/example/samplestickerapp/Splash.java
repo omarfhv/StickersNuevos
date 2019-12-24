21edc0a637fcc47d44e297c06010d4982c0e45f5
omarfhv <34494215+omarfhv@users.noreply.github.com>
2019-12-24 01:58:42 -0600

    Initial commit

---
+++ Android/app/src/main/java/com/example/samplestickerapp/Splash.java
@@ -0,0 +1,29 @@
+package com.example.samplestickerapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash.this, EntryActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 1050);
+
+    }
+}
